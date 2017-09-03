741d9df959da139342b20989fa549448d1ff6141
Moshe Immerman <moshe@egis-software.com>
2017-09-03 22:05:20 +0200

    fix osx build

--- osx/install.sh
+++ osx/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ "$(uname -s)" != "Darwin" ] && exit 0
 
-brew install pip
-pip install pyp
+sudo easy_install pip
+sudo pip install pyp
 cat brew.txt | pyp "' '.join(pp)" | xargs brew install
