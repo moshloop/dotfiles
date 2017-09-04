ab7a78ef7eac9d8a47590bfc15bd2f538df182aa
Moshe Immerman <moshe@egis-software.com>
2017-09-04 10:41:53 +0200

    fix build

--- linux/install.sh
+++ linux/install.sh
@@ -2,7 +2,8 @@
 [ "$(uname -s)" != "Linux" ] && exit 0
 
 sudo apt-get install -y libhttp-parser2.1 sudo unzip python-pip
-pip install pyp
+sudo pip install pyp
 wget -O ~/exa.zip https://github.com/ogham/exa/releases/download/v0.7.0/exa-linux-x86_64-0.7.0.zip
-sudo unzip -p ~/exa.zip > /usr/local/bin/exa
+sudo unzip -p ~/exa.zip > exa
+sudo mv exa /usr/local/bin/exa
 sudo chmod +x /usr/local/bin/exa
--- osx/install.sh
+++ osx/install.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 [ "$(uname -s)" != "Darwin" ] && exit 0
 
-sudo easy_install pip
-sudo pip install pyp
-cat brew.txt | pyp "' '.join(pp)" | xargs brew install
+# sudo easy_install pip
+# sudo pip install pyp
+pkgs=`brew list -1`
+
+
+for pkg in `cat brew.txt`; do
+    if [[ $pkgs == *"$pkg"* ]]; then
+        echo $pkg already installed
+    else
+        echo install $pkg
+        brew install $pkg
+    fi
+done
