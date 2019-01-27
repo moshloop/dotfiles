420edb70b10058b311687294686d3f4673cd0ffd
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2019-01-27 20:48:20 +0200

    fix install

--- osx/install.sh
+++ osx/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 [ "$(uname -s)" != "Darwin" ] && exit 0
 
+rm -rf $HOME/Library/Scripts
 ln -s $HOME/.dotfiles/osx/Scripts $HOME/Library/Scripts
 brew install $(cat ~/.dotfiles/osx/brew.txt)
 brew cask install $(cat ~/.dotfiles/osx/cask.txt)
--- python/install.sh
+++ python/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+brew install pip
 pip install pyp
 cat $HOME/.dotfiles/python/pip.txt | pyp "' '.join(pp)" | xargs sudo pip install
